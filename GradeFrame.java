01c0533cc9bb12fdc309884e0a4ca788ddba7336
ProgramExercise征戰 <good258johnny@yahoo.com.tw>
2014-10-18 12:14:21 +0800

    加入排序方法
    1.加入InsertionSort
    2.加入QuickSort

--- GradeFrame.java
+++ GradeFrame.java
@@ -13,6 +13,8 @@ import java.awt.TextArea;
 import java.io.*;
 import java.util.Scanner;
 
+
+
 public class GradeFrame extends JFrame {
 
 	private static JPanel contentPane;
@@ -22,9 +24,12 @@ public class GradeFrame extends JFrame {
 	static TextArea textArea; // display sort result.
 	static TextArea textArea_1; // display search result.
 	static TextArea textArea_2; // display the process.
-	JButton btnNewButton = new JButton("\u8B80\u53D6\u6A94\u6848");
-	JButton btnNewButton_1 = new JButton("\u5132\u5B58\u7D50\u679C");
-	JButton btnNewButton_2 = new JButton("\u59D3\u540D\u6392\u5E8F");
+	JButton btnNewButton = new JButton("\u8B80\u53D6\u6A94\u6848"); // click it can read the file
+	JButton btnNewButton_1 = new JButton("\u5132\u5B58\u7D50\u679C"); // click it can store the result in a file
+	JButton btnNewButton_2 = new JButton("\u59D3\u540D\u6392\u5E8F"); // click it can do Merge sort base on name ASCII code
+	JButton btnNewButton_3 = new JButton("math\u6392\u5E8F"); // click it can do Heap sort base on math grade
+	JButton btnNewButton_4 = new JButton("English\u6392\u5E8F"); // click it can do Quick sort base on english grade
+	JButton btnNewButton_5 = new JButton("\u5E73\u5747\u6392\u5E8F"); // click it can do insertion sort base on average grade evaluted by math and english grade
 
 
 	/**
@@ -90,15 +95,15 @@ public class GradeFrame extends JFrame {
 		btnNewButton_2.setBounds(10, 98, 96, 23);
 		panel.add(btnNewButton_2);
 		
-		JButton btnNewButton_3 = new JButton("math\u6392\u5E8F");
+		
 		btnNewButton_3.setBounds(116, 98, 106, 23);
 		panel.add(btnNewButton_3);
 		
-		JButton btnNewButton_4 = new JButton("English\u6392\u5E8F");
+		
 		btnNewButton_4.setBounds(232, 98, 106, 23);
 		panel.add(btnNewButton_4);
 		
-		JButton btnNewButton_5 = new JButton("\u5E73\u5747\u6392\u5E8F");
+		
 		btnNewButton_5.setBounds(348, 98, 87, 23);
 		panel.add(btnNewButton_5);
 		
@@ -139,16 +144,51 @@ public class GradeFrame extends JFrame {
 		panel.add(textArea_2);
 	}
 
+	static String[] SortDataBaseOnIntArray(int[] originalArray, int[] sortedArray, String[] dataArray){
+		for(int i = 1; i < originalArray.length; i++){
+			for(int u = 1; u < sortedArray.length; u++){
+				System.out.println("sortedArray[" + i + "] is: " + sortedArray[i] + " originalArray[" + u + "] is: " + originalArray[u]);
+				if(sortedArray[i] == originalArray[u]){
+					System.out.println("u = " + u + "i = " + i);
+					String temp = dataArray[i];
+					dataArray[i] = dataArray[u];
+					dataArray[u] = temp;
+					int temp1 = originalArray[i];
+					originalArray[i] = originalArray[u];
+					originalArray[u] = temp1;
+					originalArray[i] = 0;
+					break;
+				}
+			}
+		}
+		return dataArray;
+	} // end SortDataBaseOnIntArray method.
+
 
 }
 
+/**
+ *	class handleSort: The class have the sort method that you can input array which you needs to sort.
+ *		HeapSort:
+ *			1.HeapSort(int[] A)
+ *			2.BuildHeap(int[] A)
+ *			3.Heapify(int[] A, int i)
+ *			4.setHeapSize(int newHeapSize)
+ *			5.getHeapSize()
+ *		InsertionSort:
+ *			1.InsertionSort(int[] A)
+ *		QuickSort:
+ *			1.QuickSort(int[] A, int p, int r)
+ *			2.Partition(int[] A, int p, int r)
+ *
+ *
+ */
 class handleSort{
+	private static int heapSize = 0;
 
 	handleSort(){
 
 	}
-
-
 
 	static String readData(String filename){
 		
@@ -200,4 +240,114 @@ class handleSort{
 
 		return stringforreadData;
 	}
+
+	static int[] HeapSort(int[] A){
+		setHeapSize(A.length);
+		A = BuildHeap(A);
+		
+		for(int k = A.length - 1; k >= 2; k--){
+			
+			int temp = A[1];
+			A[1] = A[k];
+			A[k] = temp;
+			setHeapSize(getHeapSize() - 1);
+			A = Heapify(A,1);
+		}
+		return A;
+	}
+
+	static int[] BuildHeap(int[] A){
+		
+		for(int i = (int)(Math.floor(A.length / 2)); i >= 1; i--){
+			A = Heapify(A,i);
+		}
+		
+		return A;
+	}
+
+	static int[] Heapify(int[] A, int i){
+		int l = 2 * i;
+		int r = 2 * i + 1;
+		int largest = i;
+
+		if(l < getHeapSize() && A[l] > A[i]){
+			largest = l;
+		}
+
+		if(r < getHeapSize() && A[r] > A[largest]){
+			largest = r;
+		}
+
+		if(largest != i){
+			int temp = A[largest];
+			A[largest] = A[i];
+			A[i] = temp;
+			Heapify(A,largest);
+		}else{
+			return A;
+		}
+		return A;
+	}
+
+	static void setHeapSize(int newHeapSize){
+		heapSize = newHeapSize;
+	}
+
+	static int getHeapSize(){
+		return heapSize;
+	}
+
+	static int[] InsertionSort(int[] A){
+		int key = 0;
+		int i = 0;
+		for(int j = 2; j < A.length; j++){
+			key = A[j];
+			i = j - 1;
+
+			while(i > 0 && A[i] > key){
+				A[i + 1] = A[i];
+				i = i - 1;
+			}
+			A[i + 1] = key;
+		}
+		return A;
+	}
+
+	static int[] QuickSort(int[] A, int p, int r){
+		if(p < r){
+			int q = Partition(A,p,r);
+			A = QuickSort(A,p,q);
+			A = QuickSort(A,q + 1,r);
+		}
+		return A;
+	}
+
+	static int Partition(int[] A, int p, int r){
+		int x = A[p];
+		int i = p - 1;
+		int j = r + 1;
+
+		while(true){
+			j = j - 1;
+			while(A[j] > x){
+				j = j - 1;
+			}
+
+			i = i + 1;
+
+			while(A[i] < x){
+				i = i + 1;
+			}
+
+			if(i < j){
+				int temp = A[i];
+				A[i] = A[j];
+				A[j] = temp;
+			}else{
+				return j;
+			}
+		}
+		
+	}
+
 }
